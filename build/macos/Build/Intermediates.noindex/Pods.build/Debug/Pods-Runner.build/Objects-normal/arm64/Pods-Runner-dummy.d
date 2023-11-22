86254f6c25ffddfdf112bc37394c5d88e415dc29
TruongAn <truongan261097@gmail.com>
2023-11-22 22:17:23 +0700

    Init project

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/anlnt/Documents/WORK/Flutter/test_home/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/anlnt/Documents/WORK/Flutter/test_home/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
